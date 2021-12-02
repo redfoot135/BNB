ee938e24356c99a4a90af7c08872edca47cc6c28
redfoot135 <tlswogns135@gmail.com>
2021-12-03 02:02:37 +0900

    Server Deploy

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -4,6 +4,6 @@ npm install
 npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get install authbind
-sudo touch /etc/authbind/byport/444
-sudo chown ubuntu /etc/authbind/byport/444
-sudo chmod 755 /etc/authbind/byport/444
+sudo touch /etc/authbind/byport/8000
+sudo chown ubuntu /etc/authbind/byport/8000
+sudo chmod 755 /etc/authbind/byport/8000
