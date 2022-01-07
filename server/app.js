44c301757c01e97d8448a4618db755a5e89b9352
redfoot135 <tlswogns135@gmail.com>
2022-01-07 15:39:42 +0900

    Deploy test

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
-// const https = require('https');
-// const fs = require('fs');
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// const { auth, diary, calendar, talk, userinfo } = require('./routes');
+const { auth, diary, calendar, talk, userinfo } = require('./routes');
+
+const HTTPS_PORT = 80;
 
 const app = express();
 app.use(express.json());
@@ -11,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["https://b-n-b.link"],
+    origin: ["http://localhost:3000", "http://b-n-b.link.s3-website.ap-northeast-2.amazonaws.com", "https://b-n-b.link"],
     credentials: true,
     origin: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
@@ -19,32 +21,36 @@ app.use(
 );
 
 //필요 기능 라우팅
-// app.use("/auth", auth);
-// app.use("/diary", diary);
-// app.use("/calendar", calendar);
-// app.use("/talk", talk);
-// app.use("/userinfo", userinfo);
+app.use("/auth", auth);
+app.use("/diary", diary);
+app.use("/calendar", calendar);
+app.use("/talk", talk);
+app.use("/userinfo", userinfo);
 
 // 테스트용 get, post요청
 app.get("/", (req, res) => {
+  console.log(req)
+  console.log(req.body)
   res.send("Hello BNB World!!")
 });
 
-// app.post("/", (req, res) => {
-//   res.send("Hello BNB World!!")
-// });
+app.post("/", (req, res) => {
+  console.log(req)
+  console.log(req.body)
+  res.send("Hello BNB World!!")
+});
 
 let server;
-// if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
-//   const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-//   const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-//   const credentials = { key: privateKey, cert: certificate };
+if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
+  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
 
-//   server = https.createServer(credentials, app);
-//   server.listen(HTTPS_PORT, () => console.log(`Port : ${HTTPS_PORT}, HTTPS server runnning`));
+  server = https.createServer(credentials, app);
+  server.listen(HTTPS_PORT, () => console.log(`Port : ${HTTPS_PORT}, HTTPS server runnning`));
 
-// } else {
-  server = app.listen(80, () => console.log("port : 80, HTTP server running"))
-// };
+} else {
+  server = app.listen(8080, () => console.log("port : 80, HTTP server running"))
+};
 
 module.exports = server;
